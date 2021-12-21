1451762c6cfa7b6b28bf10c20e43ac54deb8630c
davezer <davezer27@gmail.com>
2021-12-21 17:22:08 -0500

    fixed syntax issues that were throwing errors

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,15 +7,17 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    include: [{
-      model: Product,
-      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-    }]
-  }).then((dbCategoryData => res.json(dbCategoryData))
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  }).then(dbCategoryData => res.json(dbCategoryData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
-    }));
+    });
 });
 
 router.get('/:id', (req, res) => {
